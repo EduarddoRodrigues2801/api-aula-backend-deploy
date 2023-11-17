1e3c9cee7d6cfd30220524b91df4b48949815748
EduarddoRodrigues2801 <eduarddo.r@gmail.com>
2023-11-17 13:12:01 -0300

    adicionado array stacks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 
+const stacks = ['Backend', 'Fullstack', 'Frontend']
+
 const app = express();
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  return res.json("Api está ok");
+  return res.json(stacks);
 });
 
 const port = process.env.PORT || 3000;
