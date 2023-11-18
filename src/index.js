a5bdfc01ac6cf3705256eda80e32a6076b349566
EduarddoRodrigues2801 <eduarddo.r@gmail.com>
2023-11-17 21:31:45 -0300

    adicionando DB postgres

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const knex = require('./conexao')
 
-const stacks = ['Backend', 'Fullstack', 'Frontend']
 
 const app = express();
+
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  return res.json(stacks);
+  try {
+    const stacks = await knex ('stacks')
+    return res.json(stacks);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({mensagem: 'Erro interno'})
+  }
 });
 
 const port = process.env.PORT || 3000;
